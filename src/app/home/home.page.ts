29e7a4629dd2a1a247726a588c08e5de832f66a1
dsa069 <dsa069@inlumine.ual.es>
2026-03-24 15:46:27 +0000

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonCardHeader,
+} from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonCard,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  public latitude?: number;
+  public longitude?: number;
+  public accuracy?: number;
+  async ngOnInit() {
+    await this.getLocation();
+  }
+  private async getLocation() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.accuracy = position.coords.accuracy;
+    } catch (error) {
+      console.error('Error al obtener la geolocalización:', error);
+    }
+  }
 }
